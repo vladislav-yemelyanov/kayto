faff654760a479d0695fd5e9dac5acef97958e1d
Vladislav Yemelyanov <vladislav.yemelyanov.93@gmail.com>
2026-02-23 18:49:37 +0200

    upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,10 @@
-cargo set-version --bump patch;
-sh cross.sh;
-git add .;
-git commit -m "upgrade";
-git push;
-cargo package;
-cargo publish;
+#!/usr/bin/env sh
+set -euo pipefail
+
+cargo set-version --bump patch
+sh cross.sh
+git add .
+git commit -m "upgrade"
+git push
+cargo package
+cargo publish
